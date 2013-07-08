f37a7b0c7759cb8d53789cafc6daaa519f1c2cd7
Stricted <info@nexus-irc.de>
2013-07-08 17:19:06 +0200

    add sqlite3 connection class (default database file will be added later)

---
+++ lib/sqlite3.class.php
@@ -0,0 +1,51 @@
+<?php
+/* lib/sqlite3.class.php - DNS-WI
+ * Copyright (C) 2013  OWNDNS project
+ * http://owndns.me/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ */
+if (!extension_loaded("sqlite3")) die("Missing <a href=\"http://www.php.net/manual/en/book.sqlite3.php\">sqlite3</a> PHP extension."); // check if extension loaded
+class DB {
+	private static $conn = NULL;
+	
+	public static function connect($host, $user, $pw, $db) {
+		self::$conn = new SQLite3($db);
+	}
+	
+	public static function query ($res) {
+		return self::$conn->query($res);
+	}
+	
+	public static function escape ($res) {
+		return self::$conn->escapeString($res);
+	}
+	
+	public static function fetch_array ($res) {
+		return $res->fetchArray();
+	}
+	
+	public static function num_rows ($res) {
+		return count($res);
+	}
+	
+	public static function error () {
+		return self::$conn->lastErrorMsg();
+	}
+	
+	public static function unescape ($data) {
+		return $data;
+	}
+}
+?>
